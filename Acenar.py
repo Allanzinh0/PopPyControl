6ec8288f89b95f2ae5e0c63c51da11fe802665a9
Allan Matheus <51325707+Allanzinh0@users.noreply.github.com>
2019-09-05 14:10:15 -0300

    Update Acenar.py

--- Acenar.py
+++ Acenar.py
@@ -11,7 +11,7 @@ speed = 0.5
 poppy.l_shoulder_x.goto_position(90, speed, wait = False)
 poppy.l_shoulder_y.goto_position(-90, speed, wait = True)
 
-for i in [0,1,2,3,4,5,6,7,8,9]:
+for i in range(10):
     poppy.l_elbow_y.goto_position(-120, speed, wait = False)
     time.sleep(0.5)
     poppy.l_elbow_y.goto_position(-60, speed, wait = False)
