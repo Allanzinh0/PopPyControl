839ca2acb0de9bded54ace247badb7ae14bf6f7c
Allan Matheus <allan_santana2004@hotmail.com>
2019-09-04 22:19:28 -0300

    Tentativa com sucesso de fazer a poppy acenar!

--- Acenar.py
+++ Acenar.py
@@ -4,8 +4,19 @@ import time
 poppy = PoppyHumanoid(simulator = 'vrep')
 
 for motor in poppy.motors:
-    motor.complient = false
+    motor.complient = False
     
-speed = 1
+speed = 0.5
 
-poppy.
+poppy.l_shoulder_x.goto_position(90, speed, wait = False)
+poppy.l_shoulder_y.goto_position(-90, speed, wait = True)
+
+for i in [0,1,2,3,4,5,6,7,8,9]:
+    poppy.l_elbow_y.goto_position(-120, speed, wait = False)
+    time.sleep(0.5)
+    poppy.l_elbow_y.goto_position(-60, speed, wait = False)
+    time.sleep(0.5)
+
+poppy.l_elbow_y.goto_position(0, speed, wait = False)
+poppy.l_shoulder_y.goto_position(0,speed, wait = True)
+poppy.l_shoulder_x.goto_position(0,speed, wait = True)
---
+++ poppyStart.py
@@ -0,0 +1,7 @@
+from poppy.creatures import PoppyHumanoid
+import time
+
+poppy = PoppyHumanoid(simulator = 'vrep')
+
+for motor in poppy.motors:
+    motor.complient = True
