b68bebd87d80849d3aff5c45b62bd31b2c08a9fd
Benício Januário <pbjobenicii@gmail.com>
2019-09-06 10:35:06 -0300

    Adicionada feature de histórico e corrigidos erros lógicos

--- inputWindows.py
+++ inputWindows.py
@@ -8,26 +8,65 @@ poppy = PoppyHumanoid(simulator = 'vrep')
 for motor in poppy.motors:
     motor.complient = False
 
-system('cls')
+# Criando uma lista com todos os ID's
+IDs = []
+
+for motor in poppy.motors:
+    IDs.append(motor.id)
+
+# Criando a lista do historico
+
+hist = {}
+
+def clear():
+    if __name__ == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+clear()
 
 while True:
     print('-'*24)
     for motor in poppy.motors:
         print('| {:<13} id: {:<2} |'.format(str(motor.name), str(motor.id)))
-    
     print('-'*24)
+
     idMotor = input('Digite o id do motor desejado: ')
-    choice = raw_input('Deseja mover ou ver o angulo? (m|v): ')
+    choice = raw_input('Deseja mover, verificar o encoder ou acessar o historico? (m|v|h): ')
 
-    for motor in poppy.motors:
-        if str(choice) == 'v':
-            if str(motor.id) == str(idMotor):
-                print("Posicao atual: " + str(motor.present_position))
+    if choice == v:
+        if idMotor in IDs:
+            for motor in poppy.motors:
+                if idMotor == int(motor.id):
+                    print("Posicao atual: {0}".format(motor.present_position))
+                    if hist[motor.name] == 'Movido':
+                        hist[motor.name] = 'Movido e Verificado'
+                    elif not motor.name in hist:
+                        hist[motor.name] = 'Verificado'
+        else:
+            print('O ID fornecido nao esta registrado.')
+            time.sleep(2)
 
-        if str(choice) == 'm':
-            if str(motor.id) == str(idMotor):
-                angulo = input('Digite o angulo desejado: ')
-                motor.goto_position(angulo, 0.5, wait = True)
-    
+    if choice == 'm':
+        if idMotor in IDs:
+            angle = input('Digite o angulo desejado: ')
+
+            for motor in poppy.motors:
+                if idMotor == int(motor.id):
+                    motor.goto_position(angle, 0.5, wait = True)
+                    if hist[motor.name] == 'Verificado':
+                        hist[motor.name] = 'Movido e Verificado'
+                    elif not motor.name in hist:
+                        hist[motor.name] = 'Movido'
+        else:
+            print('O ID fornecido nao esta registrado')
+            time.sleep(2)
+
+    if choice == 'h':
+        print('-'*51)
+        for motor, action in hist.items():
+            print('| {:<13} de ID {:<2} foi {:<20} |'.format(str(motor), str(poppy.motor.id), str(action)))
+        print('-'*51)
     time.sleep(1)
-    system('cls')
+    clear()
