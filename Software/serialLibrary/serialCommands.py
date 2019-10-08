9b578bec0655c3ecc574d36fd5bd22c299f00139
Allan Matheus <allan_santana2004@hotmail.com>
2019-10-07 23:32:52 -0300

    Otimizado o arquivo motors.json e atualizado a library para a otimização

--- Software/serialLibrary/serialCommands.py
+++ Software/serialLibrary/serialCommands.py
@@ -11,42 +11,42 @@ def writeCommand(port, id, address, size, value): # Simplifies the hexadecimal c
     
     if size == 1:
         req = header + chr(int(value))
-        checksum = int(253 - ((0xFF + 0xFF + int(id) + length + 0x03 + int(address) + int(value))%256))
+        checksum = int(255 - ((0xFF + 0xFF + int(id) + length + 0x05 + int(address) + int(value))%256))
     elif size == 2:
         value1 = value%256
         value2 = (value - (value%256))/256
         req = header + chr(int(value1)) + chr(int(value2))
-        checksum = int(253 - ((0xFF + 0xFF + int(id) + length + 0x03 + int(address) + int(value1) + int(value2))%256))
+        checksum = int(255 - ((0xFF + 0xFF + int(id) + length + 0x05 + int(address) + int(value1) + int(value2))%256))
 
     port.write(req+chr(checksum))
 #    return readCommand(port, id, address, size)
 
-def readCommand(port, id, address, size):
-    readed = list()
-    try:
-        h1 = port.read()
-	assert ord(h1) == 255
-    except:
-	e = 'Timeout on servo ' + str(id)
-        raise ValueError('Timeout servo '+ str(id))
+# def readCommand(port, id, address, size):
+#     readed = list()
+#     try:
+#         h1 = port.read()
+# 	assert ord(h1) == 255
+#     except:
+# 	e = 'Timeout on servo ' + str(id)
+#         raise ValueError('Timeout servo '+ str(id))
     
-    try:
-        h2 = port.read()
-        origin = port.read()
-        length = ord(port.read()) - 1
-        error = ord(port.read())
+#     try:
+#         h2 = port.read()
+#         origin = port.read()
+#         length = ord(port.read()) - 1
+#         error = ord(port.read())
 
-        for i in range(length):
-            readed.append(ord(port.read()))
-            length -= 1
+#         for i in range(length):
+#             readed.append(ord(port.read()))
+#             length -= 1
 
-	    if error != 0:
-	        e = 'Error from servo ' + str(id) + ' error: ' + str(hex(error))
-	        raise ValueError('Error ' + str(hex(error)) + ' in servo ' + str(id))
+# 	    if error != 0:
+# 	        e = 'Error from servo ' + str(id) + ' error: ' + str(hex(error))
+# 	        raise ValueError('Error ' + str(hex(error)) + ' in servo ' + str(id))
 
-        return readed
-    except:
-	    raise ValueError('Critical error!')
+#         return readed
+#     except:
+# 	    raise ValueError('Critical error!')
 	
 
 def setTorque(serialPort, ID, finalState):           #Defines the Torque to a motor
@@ -93,10 +93,14 @@ def getMotorNameByID(ID):
     with open("data/motors.json") as motors:
         motorsData = json.load(motors)
     
-    if str(ID) in motorsData["motorsName"]['legs']:
-        return motorsData["motorsName"]['legs'][str(ID)]
-    elif str(ID) in motorsData["motorsName"]['torso']:
-        return motorsData["motorsName"]['torso'][str(ID)]
+    name = ""
+
+    for motor in motorsData["motors"]:
+        if ID == motor["id"]:
+            name = motor["name"]
+
+    if name != "":
+        return name
     else:
         print("Servomotor id not found!")
 
@@ -122,15 +126,16 @@ def defineAngleLimitsFromJSON(serialPortLegs, serialPortTorso):
     with open("data/motors.json") as motors:
         motorsData = json.load(motors)
     
-    idsLegs = []
-    idsTorso = []
+    namesLegs = []
+    namesTorso = []
 
-    for i in motorsData['motorsName']['legs']:
-        idsLegs.append(str(motorsData['motorsName']['legs'][i]))
-    for j in motorsData['motorsName']['torso']:
-        idsTorso.append(str(motorsData['motorsName']['torso'][j]))
+    for i in motorsData['motors']:
+        if i["type"] == "legs":
+            namesLegs.append(str(i["name"]))
+        elif i["type"] == "torso":
+            namesTorso.append(str(i["name"]))
 
-    for motorLegs in idsLegs:
+    for motorLegs in namesLegs:
         idMotor = int(motorsData['motors'][motorLegs]['id'])
         angleMin = int(motorsData['motors'][motorLegs]['angleLimits']['min'])
         angleMax = int(motorsData['motors'][motorLegs]['angleLimits']['max'])
@@ -145,7 +150,7 @@ def defineAngleLimitsFromJSON(serialPortLegs, serialPortTorso):
         print("Angle Limit Max defined for motor {}".format(str(motorsData['motors'][motorLegs]['name'])))
         time.sleep(0.1)
 
-    for motorTorso in idsTorso:
+    for motorTorso in namesTorso:
         idMotor = int(motorsData['motors'][motorTorso]['id'])
         angleMin = int(motorsData['motors'][motorTorso]['angleLimits']['min'])
         angleMax = int(motorsData['motors'][motorTorso]['angleLimits']['max'])
@@ -166,23 +171,24 @@ def setPositionPredefined(serialPortLegs, serialPortTorso, positionName):
 
     defineAngleLimitsFromJSON(serialPortLegs, serialPortTorso)
 
-    motorLegs = []
-    motorTorso = []
+    namesLegs = []
+    namesTorso = []
 
     for i in motorsData['motors']:
-        motorLegs.append(str(motorsData['motors'][i]["name"]))
-    for j in motorsData['motors']:
-        motorTorso.append(str(motorsData['motors'][j]["name"]))
+        if i["type"] == "legs":
+            namesLegs.append(str(i["name"]))
+        elif i["type"] == "torso":
+            namesTorso.append(str(i["name"]))
 
-    for legs in motorLegs:
+    for legs in namesLegs:
         motor = getMotorObject(legs)
 
-        goToPosition(serialPortLegs, int(motor["id"]), int(motor["prefabPositions"][positionName]))
+        goToPosition(serialPortLegs, int(motor["id"]), int(motor["positions"][positionName]))
         time.sleep(0.5)
 
-    for Torso in motorTorso:
+    for Torso in namesTorso:
         motor = getMotorObject(Torso)
 
-        goToPosition(serialPortTorso, int(motor["id"]), int(motor["prefabPositions"][positionName]))
+        goToPosition(serialPortTorso, int(motor["id"]), int(motor["positions"][positionName]))
         time.sleep(0.5)
         
